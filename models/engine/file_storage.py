dbf8e935fd75cd2518d22bdd54caf0a327dc7e12
N-Ghi <n.iyankijij@alustudent.com>
2024-05-12 10:06:49 +0200

    All done

---
+++ models/engine/file_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Defines FileStorage class."""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """
+    Class FileStorage
+    Represent an abstracted storage test_engine.
+
+    It serializes instances to a JSON file and deserializes
+    JSON file to instances.
+    Attributes:
+        __file_path (str): Name of the file to save objects to.
+        __objects (dict): Dictionary of instantiated objects.
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Return dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with the  key <obj_class_name>.id"""
+        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serialize __objects to JSON file __file_path."""
+        object_dict = {}
+        for obj in self.__objects:
+            object_dict[obj] = self.__objects[obj].to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(object_dict, file)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing. If the file does not
+        exist, no exception should be raised)
+        """
+        try:
+            with open(self.__file_path, 'r') as file:
+                serialized_content = json.load(file)
+                if serialized_content:
+                    for item in serialized_content.values():
+                        class_name = item['__class__']
+                        self.new(eval(class_name)(**item))
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError as e:
+            print(f"Error: {e}")
+            self.__objects = {}
+        except Exception as e:
+            print(f"Error: {e}")
+            self.__objects = {}
